d8ad59af3d306c936b32127a27f44fbbcd080955
argendo <argendo@macbook.local>
2023-01-09 17:11:41 +0300

    yadisk rest api + handling token via config

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
-import os, filetype
+import os, filetype, yadisk
+from private.config import token as TOKEN
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='secsec'
@@ -29,6 +30,8 @@ def home():
         file = form.file.data
         if filetype.guess(file).extension != '' and filetype.guess(file).extension in allowed_extensions:
             file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],file.filename))
+            y = yadisk.YaDisk(token=TOKEN)
+            y.upload(file, 'app:/'+str(file.filename))
             args="Жесть, ты только что загрузил(а) свой файл!\nИ даже без без флешки, прикинь"
         else:
             args="Что-то пошло не так, братишка/сестричка"
